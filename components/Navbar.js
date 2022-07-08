80f7d58e88aee415f33e9d8d3aedadf15a2d2e95
LogouT <r.logout@protonmail.com>
2022-07-08 14:19:46 -0300

    portfolio

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Transition } from '@headlessui/react';
+import { Link } from 'react-scroll';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className='fixed z-20 bg-white w-full'>
+        <div className='w-full'>
+          First Div
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
