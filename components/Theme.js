4cbec2a63e8b0726ec209746c73a936458dddee6
LogouT <r.logout@protonmail.com>
2022-07-13 09:10:06 -0300

    flag

--- components/Theme.js
+++ components/Theme.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { US, BR } from 'country-flag-icons/react/3x2';
 
 export default function Theme() {
   const [darkMode, setDarkMode] = useState(false);
@@ -22,17 +23,20 @@ export default function Theme() {
   };
 
   return (
-    <div>
-      <div className="shadow-lg shadow-gray-400 rounded-full p-2 bg-elements">
+    <div className="grid grid-cols-2 items-center gap-4">
+      <div className="shadow-lg shadow-gray-400 rounded-full p-2 bg-elements hover:scale-110">
         <DarkModeSwitch
-          className="dark:text-white"
+          className="dark:text-white hover:scale-110"
           checked={darkMode}
           onChange={toggleDarkMode}
           size={25}
         />
       </div>
       <Link href={router.asPath} locale={lang}>
-        <a>{lang}</a>
+        {router.locale == "pt-BR"? 
+          <US title="English" className="cursor-pointer hover:scale-110"/> :
+          <BR title="Portuguese" className="cursor-pointer hover:scale-110"/>
+        }
       </Link>
     </div>
   );
